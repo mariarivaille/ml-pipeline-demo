aa9ddb9eb28f4e0dd321a307f879cb41eb56e71c
Maria <m.rivaille@yandex.ru>
2026-03-10 02:23:30 +0300

    fix: new project

--- app/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import BaseModelService
-from .llama_service import LlamaCppService
-from .demo_service import DemoService
-
-__all__ = ["BaseModelService", "LlamaCppService", "DemoService"]
